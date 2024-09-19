5a763f1cc2ee676c47920be25a2b3ca9eda28c18
dourado-rafa <rafaeldbo@al.insper.edu.br>
2024-09-19 08:26:15 -0300

    Atualizando scraper

--- scraper.py
+++ scraper.py
@@ -1,18 +1,26 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 from database import Noticia
 
+def extract_noticia(noticia_tag:Tag) -> Noticia | None:
+    try:
+        return Noticia(
+            titulo = noticia_tag.select('a.feed-post-link')[0].findChildren('p')[0].text.strip(),
+            link = noticia_tag.select('a.feed-post-link')[0].get('href'),
+            tema = noticia_tag.select('span.feed-post-metadata-section')[0].text.strip(),
+            tempo_publicacao = noticia_tag.select('span.feed-post-datetime')[0].text.strip()
+        )
+    except Exception as e:
+        return None
 
 def scraping_g1() -> list[dict[str, str]]:
     page = requests.get('https://g1.globo.com/')
     soup = BeautifulSoup(page.content, 'html.parser')
     feed_noticias = soup.select('div.feed-root')[0].select('div.feed-post-body')
 
-    return [ Noticia(
-        titulo = noticia.select('a.feed-post-link')[0].findChildren('p')[0].text.strip(),
-        link = noticia.select('a.feed-post-link')[0].get('href').strip(),
-        tema = noticia.select('span.feed-post-metadata-section')[0].text.strip(),
-        tempo_publicacao = noticia.select('span.feed-post-datetime')[0].text.strip()
-    ) for noticia in feed_noticias ]
+    return [extract_noticia(noticia) for noticia in feed_noticias]
 
+
+if __name__ == '__main__':
+    print('\n\n'.join(f'{i}: {noticia}' for i, noticia in enumerate(scraping_g1())))
